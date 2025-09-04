deecd3097d07649672ac6defccbae3b2421f564b
anuj-8109 <anujpaterya29@gmaail.com>
2025-09-04 19:02:41 +0530

    done

--- Frontend/src/components/UserMenu.jsx
+++ Frontend/src/components/UserMenu.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 // import { Search, Bell, User } from "lucide-react";
+import { Home, Search, Bell, User } from "lucide-react";
 
 const UserMenu = () => {
    return (
     <div className="fixed bottom-0 left-0 right-0 bg-white border-t shadow-md">
       <div className="flex justify-around items-center h-16">
         
-        {/* Home */}
+        
         <button className="flex flex-col items-center text-gray-600 hover:text-blue-500">
           <Home className="h-6 w-6" />
           <span className="text-xs">Home</span>
         </button>
 
-        {/* Search */}
+     
         <button className="flex flex-col items-center text-gray-600 hover:text-blue-500">
           <Search className="h-6 w-6" />
           <span className="text-xs">Search</span>
         </button>
 
-        {/* Notifications */}
+        
         <button className="flex flex-col items-center text-gray-600 hover:text-blue-500">
           <Bell className="h-6 w-6" />
           <span className="text-xs">Alerts</span>
         </button>
 
-        {/* Profile */}
         <button className="flex flex-col items-center text-gray-600 hover:text-blue-500">
           <User className="h-6 w-6" />
           <span className="text-xs">Profile</span>
@@ -37,6 +37,6 @@ const UserMenu = () => {
 
 export default UserMenu
 
-import { Home, Search, Bell, User } from "lucide-react";
+
 
 
