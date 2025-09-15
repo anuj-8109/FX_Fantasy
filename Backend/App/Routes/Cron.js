2944d5413da8f189aa69f0d9dc58b3dace07d093
PP21790 <pankajpatidar333@gmail.com>
2025-09-15 17:58:31 +0530

    done

--- Backend/App/Routes/Cron.js
+++ Backend/App/Routes/Cron.js
@@ -1,13 +1,13 @@
 const router = require("express").Router()
 
-const {AddBulkStockCron,DeleteTokenAliceToken,TournamentStatusChange} = require('../Controllers/Cron')
+const {AddBulkStockCron,DeleteTokenAliceToken,TournamentStatusChange,updateContestRanks} = require('../Controllers/Cron')
 
 
 
 router.get('/cron/add', AddBulkStockCron);
 router.get('/cron/delete', DeleteTokenAliceToken);
 router.get('/cron/tournament-status-change', TournamentStatusChange);
-
+router.get('/cron/updatecontestranks', updateContestRanks);
 
 
 module.exports = router;
