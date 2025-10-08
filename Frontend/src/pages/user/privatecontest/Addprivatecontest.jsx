51e55d1e7842d23b412e9191c2a09afa732fdba2
Anuj Paterya <anujpaterya29@gmail.com>
2025-10-08 10:06:46 +0530

    done

--- Frontend/src/pages/user/privatecontest/Addprivatecontest.jsx
+++ Frontend/src/pages/user/privatecontest/Addprivatecontest.jsx
@@ -8,10 +8,10 @@ const AddContest = () => {
   const token = localStorage.getItem("token");
   const client_id = localStorage.getItem("userId");
   const location = useLocation();
-const tournament_id = location.state?.tournament_id; 
+  const tournament_id = location.state?.tournament_id;
 
   const initialForm = {
-    add_by:"",
+    add_by: "",
     name: "",
     description: "",
     contest_type: "",
