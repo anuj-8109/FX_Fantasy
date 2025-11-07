04d7042a7e5dbf70c1a4aba5db3e171070f998f2
PP21790 <pankajpatidar333@gmail.com>
2025-11-07 20:40:36 +0530

    done

--- Backend/App/api/Controllers/Clients.js
+++ Backend/App/api/Controllers/Clients.js
@@ -1073,7 +1073,7 @@ async  addMoneyInWallet(req, res) {
           })
           await results.save();
           client.referwamount+= receiveramount;
-          client.wamount += receiveramount;
+         // client.wamount += receiveramount;
           await client.save();
 
 
@@ -1105,7 +1105,7 @@ const socketData = {
 
           if (sender) {
             sender.referwamount+= senderamount;
-            sender.wamount += senderamount;
+           // sender.wamount += senderamount;
             await sender.save();
 
    const notificationTitleSender = 'Important Update';
@@ -1153,7 +1153,7 @@ const socketData = {
 
           // Update client's wallet amount
             client.referwamount+= receiveramount;
-          client.wamount += receiveramount;
+        //  client.wamount += receiveramount;
           await client.save();
 
    const notificationTitle = 'Important Update';
@@ -1185,7 +1185,7 @@ const socketData = {
 
           if (sender) {
               sender.referwamount+= senderamount;
-            sender.wamount += senderamount;
+        //    sender.wamount += senderamount;
             await sender.save();
 
 
