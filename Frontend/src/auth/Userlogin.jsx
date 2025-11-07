1915c93a6a271196b73ba8ff2eb5153ad8bfb4b3
Anuj Paterya <anujpaterya29@gmail.com>
2025-11-07 21:10:45 +0530

    Done

--- Frontend/src/auth/Userlogin.jsx
+++ Frontend/src/auth/Userlogin.jsx
@@ -40,7 +40,7 @@ const UserLogin = () => {
   const token = params.get("token");
   const fullName = params.get("FullName");
   const email = params.get("email");
-  const userId = params.get("id");
+  const userId = params.get("userId");
   const createdAt = params.get("createdAt");
 
   // ✅ Check if Google login returned data
