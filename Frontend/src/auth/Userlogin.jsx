aa75884f6f5171b4c535df5f9d9ccc43eeefb3a2
Anuj Paterya <anujpaterya29@gmail.com>
2025-11-07 17:17:24 +0530

    vgh

--- Frontend/src/auth/Userlogin.jsx
+++ Frontend/src/auth/Userlogin.jsx
@@ -6,7 +6,6 @@ import Swal from "sweetalert2";
 import toast from "react-hot-toast";
 import { FaFacebookF, FaGoogle } from "react-icons/fa";
 import { useLocation } from "react-router-dom";
-import  config  from "../utils/config";
 
 const UserLogin = () => {
   const location = useLocation();
@@ -35,7 +34,7 @@ const UserLogin = () => {
   });
 
   const handleGoogleLogin = () => {
-    window.location.href = `${config.base_url}api/client/google`;
+    window.location.href = "https://fx.tradestreet.in/api/client/google";
   };
 
 
